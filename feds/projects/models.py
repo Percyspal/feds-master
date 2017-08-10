b9e98669d52b76256850f856188d67609596aa41
Kieran Mathieson <mathieso@oakland.edu>
2017-08-10 10:03:05 -0400

    Created model for project app.

--- feds/projects/models.py
+++ feds/projects/models.py
@@ -1,18 +1,23 @@
+import string
+import random
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
-
+from django.core.exceptions import ValidationError
+from django.contrib import messages
 
 class Project(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        related_name='projects'
+        related_name='projects',
+        blank=False,
     )
     title = models.CharField(
         max_length=200,
         blank=False,
         help_text='E.g., ACC 450 invoicing basic'
     )
+    # TODO: Add model validation to check whether the slug is unique.
     slug = models.SlugField(
         max_length=200,
         blank=True,
@@ -22,7 +27,7 @@ class Project(models.Model):
         blank=True,
         help_text='Remind your future self what this project is about.'
     )
-    created = models.DateField(
+    when_created = models.DateField(
         auto_now_add=True,
         db_index=True
     )
@@ -35,4 +40,58 @@ class Project(models.Model):
         # TODO: check for uniqueness within user?
         if not self.slug:
             self.slug = slugify(self.title)
+        # Adjust slug if another project for this user is already using that slug.
+        self.make_slug_unique_for_user()
         super().save(*args, **kwargs)
+
+    def make_slug_unique_for_user(self):
+        """ Make sure that the slug is unique for the projects owned by this user. """
+        slug_ok = False
+        slug_changed = False # Show whether the code changed the slug.
+        while not slug_ok:
+            # Find project with the current slug for the project's user.
+            projects_with_slug = Project.objects.filter(user=self.user, slug=self.slug)
+            # If there is more than one, there's a problem.
+            if projects_with_slug.count() > 1:
+                raise ValidationError(
+                    'Danger, Will Robinson! Too many slugs for user {user_id} with value {slug}!'
+                        .format(user_id=self.user.pk, slug=self.slug)
+                )
+            # Found any?
+            if projects_with_slug.count() == 0:
+                # No, so slug is OK.
+                slug_ok = True
+                # Exit slug checking loop
+                break
+            if projects_with_slug.count() == 1:
+                # Found one project with the slug.
+                if self.pk is None:
+                    # Saving a new record, so slug is already being used.
+                    # Append stuff, to try to make the slug unique.
+                    self.add_slug_extra_piece()
+                    slug_ok = False
+                    # Flag that the slug was changed.
+                    slug_changed = True
+                    # Try the check again.
+                    continue
+                # Slug already exists, and this is not a new project.
+                # Is the project with the slug the same as self?
+                project_with_slug = projects_with_slug[0]
+                if project_with_slug.pk == self.pk:
+                    # Same thing. No problem.
+                    slug_ok = True
+                    # Exit slug checking loop.
+                    break
+                # The project with the slug is not self.
+                # Append stuff, to try to make the slug unique.
+                self.add_slug_extra_piece()
+                # Flag that the slug was changed.
+                slug_changed = True
+        # Was the slug changed?
+        if slug_changed:
+            # Tell the user.
+            messages.info('The slug was changed to keep it unique.')
+
+    def add_slug_extra_piece(self):
+        """ Add an extra piece to a slug to try to make it unique. """
+        self.slug += '-' + random.choice(string.ascii_lowercase)
