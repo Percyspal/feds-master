c4483050640dd7f0a7782ad08916cd7cf1a7e508
Kieran Mathieson <mathieso@oakland.edu>
2017-08-14 18:11:43 -0400

    Business area field, removed params field.

--- feds/projects/forms.py
+++ feds/projects/forms.py
@@ -1,12 +1,19 @@
 from django import forms
+from feds.settings import FEDS_REST_HELP_URL
+from businessareas.models import BusinessArea
 from .models import Project
 
 
 class ProjectForm(forms.ModelForm):
+    # Specify business_area field, so can omit MT choice from <select>.
+    business_area = forms.ModelChoiceField(
+        BusinessArea.objects.all().order_by('title'),
+        empty_label=None
+    )
 
     class Meta:
         model = Project
-        fields = ('title', 'slug', 'description', )
+        fields = ('title', 'slug', 'description', 'business_area', )
 
 
 class ConfirmDeleteForm(forms.Form):
--- feds/projects/models.py
+++ feds/projects/models.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.utils.text import slugify
 from django.core.exceptions import ValidationError
 from feds.settings import FEDS_REST_HELP_URL
+from businessareas.models import BusinessArea
+from jsonfield import JSONField
 
 
 # TODO: Add project-level anomalies?
@@ -17,6 +19,13 @@ class Project(models.Model):
         settings.AUTH_USER_MODEL,
         related_name='projects',
         blank=False,
+    )
+    business_area = models.ForeignKey(
+        BusinessArea,
+        related_name='project_business_area',
+        blank=False,
+        null=False,
+        help_text='What business area does this project use?',
     )
     # TODO: check that Django trims the field before the blank check.
     title = models.CharField(
@@ -39,6 +48,12 @@ class Project(models.Model):
         auto_now=True,
         db_index=True
     )
+    # project_params = JSONField(
+    #     blank=True,
+    #     default={},
+    #     help_text='''Parameters for this project. JSON. Copied from the
+    #               business area when the project is created.'''
+    # )
 
     def __str__(self):
         return self.title
@@ -112,3 +127,90 @@ class Project(models.Model):
     def add_slug_extra_piece(self):
         """ Add an extra piece to a slug to try to make it unique. """
         self.slug += '-another'
+
+
+class ProjectSetting(models.Model):
+    """ A setting that a project can have. """
+    SETTING_TYPES = (
+        ('setting', 'Normal setting'),
+        ('anomaly', 'Anomaly'),
+    )
+    title = models.CharField(
+        max_length=200,
+        blank=False,
+        help_text='Title of this setting.'
+    )
+    description = models.TextField(
+        blank=True,
+        help_text='Description of this setting.'
+    )
+    setting_type = models.CharField(
+        max_length=10,
+        blank=False,
+        choices=SETTING_TYPES,
+        help_text='What type of setting is this?'
+    )
+    setting_params = JSONField(
+        blank=True,
+        default={},
+        help_text='Parameters for this setting. JSON.'
+    )
+
+    def __str__(self):
+        return self.title
+
+    # def anomalize_data(self, data_set):
+    #     pass
+
+
+class AvailableProjectSetting(models.Model):
+    """ A setting available for projects in a business area. """
+    business_area = models.ForeignKey(
+        BusinessArea,
+        blank=False,
+        null=False,
+        help_text='What business area is this project setting availble for?'
+    )
+    project_setting = models.ForeignKey(
+        ProjectSetting,
+        null=False,
+        blank=False,
+        help_text='The project setting that is available.'
+    )
+    project_setting_order = models.IntegerField(
+        null=False,
+        blank=False,
+        help_text='Order of the setting in the settings list for the project.'
+    ),
+    project_setting_params = JSONField(
+        blank=True,
+        default={},
+        help_text='JSON parameters to initialize the project setting.'
+    )
+
+    def __str__(self):
+        return '{setting} for projects in {business_area}'.format(
+            setting=self.project_setting.title,
+            business_area=self.business_area.title
+        )
+
+
+class SelectedProjectSetting(models.Model):
+    """ A project setting for a particular project. """
+    project = models.ForeignKey(
+        Project,
+        null=False,
+        blank=False,
+        help_text='Project the setting data is for.'
+    )
+    available_project_setting = models.ForeignKey(
+        AvailableProjectSetting,
+        null=False,
+        blank=False,
+        help_text='Available project setting the data is for.'
+    )
+    setting_params = JSONField(
+        blank=True,
+        default={},
+        help_text='JSON parameters for this project setting, for this project.'
+    )
