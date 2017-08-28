b97e4f8499f4e93e8ef1cd1d4b7a70727ea328c6
Kieran Mathieson <mathieso@oakland.edu>
2017-08-28 12:34:27 -0400

    Fixed generate URL

--- feds/generate/urls.py
+++ feds/generate/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import generate
+from .views import generate, delete_archive
 
 app_name = 'generate'
 urlpatterns = [
-   url(r'^(?P<project_id>[0-9]+)/$', generate, name='generate'),
+   url(r'^$', generate, name='generate'),
+   url(r'^ajax/deletearchive/$', delete_archive, name='delete_archive'),
 ]
